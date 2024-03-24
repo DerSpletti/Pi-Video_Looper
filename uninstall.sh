3dfc3a8c6b8c0e0768926d37753993ba964a048b
DerSpletti <96838084+DerSpletti@users.noreply.github.com>
2024-03-24 20:59:26 +0100

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-# Benutzername abfragen
+# Benutzername abfragen, der für die Installation verwendet wurde
 read -p "Bitte geben Sie den Benutzernamen ein, der für die Installation verwendet wurde: " username
 
-# Systemd Service stoppen und deaktivieren
-sudo systemctl stop usb-vlc-autoplay.service
-sudo systemctl disable usb-vlc-autoplay.service
+# Stoppe und deaktiviere den systemd Service für Autoplay
+sudo systemctl stop usb-autoplay.service
+sudo systemctl disable usb-autoplay.service
 
-# systemd Service-Datei entfernen
-sudo rm -f /etc/systemd/system/usb-vlc-autoplay.service
+# Entferne den systemd Service für Autoplay
+sudo rm -f /etc/systemd/system/usb-autoplay.service
 
-# Autoplay-Skript entfernen
+# Entferne das Autoplay-Skript
 autoplay_script_path="/home/$username/usb-vlc-playback.py"
 rm -f "$autoplay_script_path"
 
-# Systemd neu laden, um Änderungen zu übernehmen
+# Entferne die Autologin-Konfiguration
+sudo rm -f /etc/systemd/system/getty@tty1.service.d/override.conf
+
+# Setze das Standard-Target zurück, falls notwendig
+# sudo systemctl set-default graphical.target # Nur ausführen, wenn das Standard-Target geändert wurde
+
+# Lade die systemd Konfiguration neu
 sudo systemctl daemon-reload
 sudo systemctl reset-failed
 
-echo "Deinstallation abgeschlossen. Die Änderungen wurden rückgängig gemacht."
+echo "Deinstallation abgeschlossen. Alle Änderungen wurden rückgängig gemacht."
