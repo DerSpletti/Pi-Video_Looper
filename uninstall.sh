aa180b4a5442118a7131a26dbae60475e2afd0cd
DerSpletti <96838084+DerSpletti@users.noreply.github.com>
2024-03-26 20:18:40 +0100

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# Stoppen und Deaktivieren des systemd Service
+# Stoppen und Deaktivieren des systemd Services
 echo "Stoppe und deaktiviere systemd Service für Autoplay..."
 sudo systemctl stop usb-autoplay.service
 sudo systemctl disable usb-autoplay.service
 sudo rm /etc/systemd/system/usb-autoplay.service
 
-# Entfernen des Autoplay-Skripts
-echo "Entferne USB-Autoplay-Skript..."
-read -p "Bitte geben Sie den Benutzernamen ein, der für die Installation verwendet wurde: " username
+# Entfernen des USB-Autoplay-Skripts
+echo "Bitte geben Sie den Benutzernamen ein, der für die Installation verwendet wurde: "
+read username
 autoplay_script_path="/home/$username/usb-vlc-playback.py"
 rm -f "$autoplay_script_path"
 
 # Entfernen des Mount-Punktes
+echo
