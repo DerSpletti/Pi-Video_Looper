8a7dc35d42fb46c2c53c892c0ac2923705e3584e
DerSpletti <96838084+DerSpletti@users.noreply.github.com>
2024-04-01 18:50:30 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
+# Fordere den Benutzernamen an, der für die Installation verwendet wurde
+read -p "Bitte geben Sie den Benutzernamen ein, der für die Installation verwendet wurde: " username
+
 # Stoppen und Deaktivieren des systemd Services
 echo "Stoppe und deaktiviere systemd Service für Autoplay..."
 sudo systemctl stop usb-autoplay.service
 sudo systemctl disable usb-autoplay.service
-sudo rm /etc/systemd/system/usb-autoplay.service
+sudo rm -f /etc/systemd/system/usb-autoplay.service
+
+# systemd Konfiguration neu laden und zurücksetzen
 sudo systemctl daemon-reload
 sudo systemctl reset-failed
 
 # Entfernen des USB-Autoplay-Skripts
-echo "Entferne USB-Autoplay-Skript..."
-read -p "Bitte geben Sie den Benutzernamen ein, der für die Installation verwendet wurde: " username
 autoplay_script_path="/home/$username/usb-vlc-playback.py"
+echo "Entferne das USB-Autoplay-Skript..."
 rm -f "$autoplay_script_path"
 
 # Entfernen des Mount-Punktes
-echo "Entferne Mount-Punkt /mnt/usb..."
+echo "Entferne den Mount-Punkt /mnt/usb..."
 sudo rm -rf /mnt/usb
 
 # Entfernen der Autologin-Konfiguration
-echo "Entferne Autologin-Konfiguration..."
-sudo rm -rf /etc/systemd/system/getty@tty1.service.d/override.conf
-sudo systemctl daemon-reload
+echo "Entferne die Autologin-Konfiguration..."
+sudo rm -f /etc/systemd/system/getty@tty1.service.d/override.conf
 
 echo "Deinstallation abgeschlossen. Alle vorgenommenen Änderungen wurden rückgängig gemacht."
